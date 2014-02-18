8b7acdb98d615d5a2c9a241bb921b44d25e34a2a
Robert de Bath <rdebath@tvisiontech.co.uk>
2014-02-18 19:27:00 +0000

    Improve automatic version script

--- tritium/bfi.version.c
+++ tritium/bfi.version.c
@@ -3,19 +3,13 @@
 #include "bfi.tree.h"
 #include "bfi.version.h"
 
-#define VERSION_MAJOR	0
-#define VERSION_MINOR	10
-/* Commit number for last MINOR build */
-#define MINOR_BUILD	129
-
 #define VS_Q(x) #x
 #define VS_S(x) VS_Q(x)
 #define VS(x) VS_S(VERSION_ ## x)
-#define VERSION VS(MAJOR) "." VS(MINOR)
+#define VERSION VS(MAJOR) "." VS(MINOR) "." VS(BUILD) VERSION_SUFFIX
 
 void
 print_banner(FILE * fd, char const * program)
 {
-    fprintf(fd, "%s: Version "VERSION".%d%s, date "BUILD_DATE"\n",
-	    program, VERSION_BUILD-MINOR_BUILD, VERSION_SUFFIX);
+    fprintf(fd, "%s: Version "VERSION", date "BUILD_DATE"\n", program);
 }
--- tritium/version.sh
+++ tritium/version.sh
@@ -1,25 +1,54 @@
 #!/bin/sh
 [ -n "$1" ] && cd $1
 
-git_status() {
-    VER=`git rev-list origin | wc -l`
-    DIFF=`git rev-list origin..HEAD | wc -l`
-    MOD=`git status --porc | grep "^ M" | wc -l`
+MAJOR=0
+MINOR=10
+# Set below blank for the commit to be tagged with the above version.
+SUFFIX="-post"
+BUILD=0
 
-    echo Origin commits: $VER, Local commits: $DIFF, Local edits: $MOD 1>&2
+git_status() {
+    TAG=`git tag -l v$MAJOR.$MINOR`
+    if [ `git rev-list origin -n 1 2>/dev/null | wc -l` -eq 0 ]
+    then ORIGIN=HEAD
+    else ORIGIN=origin
+    fi
 
     SUFFIX=
+    VER=`git rev-list HEAD ${TAG:+^$TAG} | wc -l`
+    OVER=`git rev-list $ORIGIN ${TAG:+^$TAG} | wc -l`
+    DIFF=`git rev-list $ORIGIN..HEAD | wc -l`
+    NDIFF=`git rev-list HEAD..$ORIGIN | wc -l`
+
+    MOD=`git status --porc | grep "^ M" | wc -l`
+
+    HDR=`( git rev-list HEAD -n 1 ; git rev-list $ORIGIN -n 1 ) 2>/dev/null | uniq | wc -l`
+
+    [ "$DIFF" -eq "0" -a "$NDIFF" -eq 0 -a "$HDR" -eq 2 ] && {
+	echo WARNING: YOUR VERSION IS FORKED 1>&2
+	SUFFIX="-FORK"
+    }
+
+    VER=$((VER-DIFF))
+
+    echo Origin ver: $MAJOR.$MINOR.$VER, Local commits: $DIFF, Local edits: $MOD 1>&2
+    [ "$OVER" -gt "$VER" ] &&
+	echo WARNING: Origin is ahead of your current HEAD by $((OVER-VER)) commits. 1>&2
+
     [ "$DIFF" -ne 0 ] && SUFFIX="$SUFFIX.$DIFF"
     [ "$MOD" -ne 0 -a "$DIFF" -eq 0 ] && SUFFIX="${SUFFIX}.0"
     [ "$MOD" -ne 0 ] && SUFFIX="${SUFFIX}.${MOD}"
 
+    [ "$TAG" = "" ] && SUFFIX="$SUFFIX-pre"
+
     SUFFIX="$SUFFIX `git rev-list HEAD -n 1 | cut -c 1-7`"
+    BUILD="$VER"
 }
 
-VER=MINOR_BUILD
-SUFFIX=unknown
 ((type git && git status ) >/dev/null 2>&1) && git_status
 
-echo "#define VERSION_BUILD    $VER"
+echo "#define VERSION_MAJOR    $MAJOR"
+echo "#define VERSION_MINOR    $MINOR"
+echo "#define VERSION_BUILD    $BUILD"
 echo "#define VERSION_SUFFIX   \"$SUFFIX\""
 echo "#define BUILD_DATE       \"`date --rfc-3339=seconds`\""
