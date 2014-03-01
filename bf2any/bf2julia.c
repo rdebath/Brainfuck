f4d65581e534bf346deaae5a71d7e7109e1e45a9
Robert de Bath <rdebath@tvisiontech.co.uk>
2014-03-01 13:56:10 +0000

    Improved julia speed, a little.

--- bf2any/bf2julia.c
+++ bf2any/bf2julia.c
@@ -10,13 +10,33 @@
  *
  * But is VERY variable presumably due to the dynamic JIT.
  * Without the function it only manages 20,000,000 instructions per second.
- * (Probably because it only JITs functions)
+ *
+ * This is because Julia only properly compiles functions, however,
+ * it takes a very long time to compile large functions and programs.
+ *
+ * LostKng.bf is unrunnable.
  */
+
+#define MAXWHILE 256
+
+struct instruction {
+    int ch;
+    int count;
+    int ino;
+    int icount;
+    struct instruction * next;
+    struct instruction * loop;
+    char * cstr;
+} *pgm = 0, *last = 0, *jmpstack = 0;
+
+void loutcmd(int ch, int count, struct instruction *n);
 
 int ind = 0;
 #define I printf("%*s", ind*4, "")
+static int lblcount = 0;
+static int icount = 0;
 
-static void print_cstring(void);
+static void print_cstring(char * str);
 static int no_function = 0;
 
 int
@@ -41,20 +61,117 @@ check_arg(const char * arg)
 void
 outcmd(int ch, int count)
 {
+    struct instruction * n = calloc(1, sizeof*n), *n2;
+    if (!n) { perror("bf2any"); exit(42); }
+
+    icount ++;
+    n->ch = ch;
+    n->count = count;
+    n->icount = icount;
+    if (ch == '"')
+	n->cstr = strdup(get_string());
+    if (!last) {
+	pgm = n;
+    } else {
+	last->next = n;
+    }
+    last = n;
+
+    if (n->ch == '[') {
+	n->ino = ++lblcount;
+	n->loop = jmpstack; jmpstack = n;
+    } else if (n->ch == ']') {
+	n->loop = jmpstack; jmpstack = jmpstack->loop; n->loop->loop = n;
+    }
+
+    if (ch != '~') return;
+
+    if (icount == 3 && pgm->ch == '!' && pgm->next->ch == '"' &&
+	    pgm->next->next->ch == '~') {
+	loutcmd('"', 0, pgm->next);
+    } else {
+
+	loutcmd(999, 1, n);
+
+	if (icount < MAXWHILE || no_function) {
+	    for(n=pgm; n; n=n->next)
+		loutcmd(n->ch, n->count, n);
+	} else {
+	    for(n=pgm; n; n=n->next) {
+		if (n->ch != ']') continue;
+		if (n->icount-n->loop->icount <= MAXWHILE) continue;
+		loutcmd(1000, 1, n->loop);
+		for(n2 = n->loop->next; n != n2; n2=n2->next) {
+		    if (n2->ch == '[' && n2->loop->icount-n2->icount > MAXWHILE) {
+			loutcmd(n2->ch, n2->count, n);
+			loutcmd(1002, n2->ino, n);
+			n2 = n2->loop;
+			loutcmd(n2->ch, n2->count, n);
+		    } else
+			loutcmd(n2->ch, n2->count, n2);
+		}
+		loutcmd(1001, 1, n);
+	    }
+
+	    for(n=pgm; n; n=n->next) {
+		if (n->ch != '[' || n->loop->icount-n->icount <= MAXWHILE)
+		    loutcmd(n->ch, n->count, n);
+		else {
+		    loutcmd(n->ch, n->count, n);
+		    loutcmd(1002, n->ino, n);
+		    n=n->loop;
+		    loutcmd(n->ch, n->count, n);
+		}
+	    }
+	}
+
+    }
+
+    while(pgm) {
+	n = pgm;
+	pgm = pgm->next;
+	if (n->cstr)
+	    free(n->cstr);
+	memset(n, '\0', sizeof*n);
+	free(n);
+    }
+}
+
+void
+loutcmd(int ch, int count, struct instruction *n)
+{
     switch(ch) {
+    case 999:
+	if (bytecell)
+	    printf( "%s%d%s",
+		    "m = zeros(Uint8, 65536)\n"
+		    "p = ", BOFF, "\n");
+	else
+	    printf( "%s%d%s",
+		    "m = zeros(typeof(0), 65536)\n"
+		    "p = ", BOFF, "\n");
+	break;
+    case 1000:
+	printf("function bf%d(m,p)\n", n->ino);
+	ind++;
+	break;
+    case 1001:
+	ind--;
+	printf("end\n\n");
+	break;
+    case 1002:
+	I; printf("bf%d(m,p)\n", count);
+	break;
+
     case '!':
-	if (!no_function) puts("function brainfuck()");
-	printf( "%s%d%s",
-		"m = zeros(typeof(0), 65536)\n"
-		"p = ", BOFF, "\n");
+	if (!no_function) puts("function brainfuck(m,p)");
 	break;
     case '~':
-	if (!no_function) puts("end\nbrainfuck()");
+	if (!no_function) puts("end\nbrainfuck(m,p)");
 	break;
 
     case '=': I; printf("m[p] = %d\n", count); break;
     case 'B':
-	if(bytecell) { I; printf("m[p] &= 255\n"); }
 	I; printf("v = m[p]\n");
 	break;
     case 'M': I; printf("m[p] = m[p]+v*%d\n", count); break;
@@ -72,24 +189,21 @@ outcmd(int ch, int count)
     case '<': I; printf("p -= %d\n", count); break;
     case '>': I; printf("p += %d\n", count); break;
     case '[':
-	if(bytecell) { I; printf("m[p] &= 255\n"); }
 	I; printf("while m[p] != 0\n");
 	ind++;
 	break;
     case ']':
-	if(bytecell) { I; printf("m[p] &= 255\n"); }
 	ind--; I; printf("end\n");
 	break;
     case '.': I; printf("print(char(m[p]))\n"); break;
-    case '"': print_cstring(); break;
+    case '"': print_cstring(n->cstr); break;
     case ',': I; printf("m[p] = read(STDIN, Char)\n"); break;
     }
 }
 
 static void
-print_cstring(void)
+print_cstring(char * str)
 {
-    char * str = get_string();
     char buf[256];
     int gotnl = 0;
     size_t outlen = 0;
