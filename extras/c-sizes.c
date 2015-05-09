e3696fab6fd21ac101c7e876d86bd3fc1fb18e46
Robert de Bath <rdebath@tvisiontech.co.uk>
2015-05-09 03:00:00 +0000

    Update c-sizes.c

--- extras/c-sizes.c
+++ extras/c-sizes.c
@@ -1,10 +1,27 @@
+
+/* Make printf work correctly for the C standards */
+#if (defined(_WIN32) && defined(__MINGW32__) ) && !defined(__USE_MINGW_ANSI_STDIO)
+/* #define printf __mingw_printf */
+#define __USE_MINGW_ANSI_STDIO 1
+#endif
+
+#ifdef __STRICT_ANSI__
+#ifndef __STDC_VERSION__
+#define inline
+#endif
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <limits.h>
 #include <float.h>
-#include <stdint.h>
+#include <time.h>
+#if _POSIX_VERSION >= 199506L || defined(LLONG_MAX)
 #include <inttypes.h>
+#endif
 
+#ifndef PRIdLLONG
 /* These are mostly for Win32, but probably still a good idea elsewhere. */
 #if defined(LLONG_MAX) && defined(INT64_MAX) && LLONG_MAX == INT64_MAX
 #define PRIdLLONG PRId64
@@ -17,11 +34,6 @@
 #else
 #define PRIuLLONG "llu"
 #endif
-
-/* Make printf work correctly for the C standards */
-/* See also: -D__USE_MINGW_ANSI_STDIO */
-#if (defined(_WIN32) && defined(__MINGW32__) && defined(__SIZEOF_LONG_DOUBLE__) ) && !defined(__USE_MINGW_ANSI_STDIO)
-#define printf __mingw_printf
 #endif
 
 /*
@@ -58,7 +70,11 @@ is_big_endian(void)
 }
 
 int
+#ifdef BROKEN_MAIN
 main(int argc, char ** argv)
+#else
+main(void)
+#endif
 {
     int e = is_big_endian();
 #if !defined(CHAR_BIT) || !defined(CHAR_MIN) || !defined(CHAR_MAX)
@@ -74,12 +90,44 @@ main(int argc, char ** argv)
     else
 	printf("LITTLE ENDIAN");
 
-    printf(" %d:%d:%d:%d %d\n",
+    printf(" C%d:S%d:I%d:L%d P%d\n",
 	(int)sizeof(char), (int)sizeof(short), (int)sizeof(int),
 	(int)sizeof(long), (int)sizeof(char*));
 
     if ( !(~1==-2) )
 	printf("WARNING: This is not a twos-complement machine\n\n");
+
+    {
+        int y=0, x, flg = sizeof(int) * CHAR_BIT +2;
+        for(x=1;x>y && --flg;y=x,x=(x|(x<<1)));
+	x = sizeof(int) * CHAR_BIT +2 - flg;
+        if (x >= sizeof(int) * CHAR_BIT)
+            printf("WARNING: Signed integers are insane (too big for their bit size).\n");
+	else if (x != sizeof(int) * CHAR_BIT -1)
+	    printf("WARNING: Signed integers have %d magnitude bits.\n", x);
+    }
+
+#ifdef __STDC_VERSION__
+    printf("Macro __STDC_VERSION__     %ldL\n", (long) __STDC_VERSION__);
+#else
+#ifndef __STDC__
+    printf("__STDC__ NOT defined\n");
+#endif
+#endif
+
+#ifdef _POSIX_VERSION
+    printf("Macro _POSIX_VERSION       %ld\n", (unsigned long) _POSIX_VERSION);
+#else
+    printf("Macro _POSIX_VERSION not defined!\n");
+#endif
+
+#ifdef _POSIX2_VERSION
+    printf("Macro _POSIX2_VERSION      %ld\n", (unsigned long) _POSIX2_VERSION);
+#endif
+
+#ifdef _XOPEN_VERSION
+    printf("Macro _XOPEN_VERSION       %ld\n", (unsigned long) _XOPEN_VERSION);
+#endif
 
     if (sizeof(void *) == sizeof(int)) {
 	printf("Bytes PT%2d, Pointer        int pointer\n",
@@ -88,15 +136,14 @@ main(int argc, char ** argv)
 	printf("Bytes PT%2d, Pointer <***** long pointer\n",
 	       (int)sizeof(void *));
     } else if (sizeof(void *) >= sizeof(long)) {
-	printf("Bytes PT%2d, Pointer <***** HUGE MODEL pointer\n",
+	printf("Bytes PT%2d, Pointer <***** HUGE pointer\n",
 	       (int)sizeof(void *));
     } else {
-	printf("Bytes PT%2d, Pointer <***** WTF pointer\n",
+	printf("Bytes PT%2d, Pointer <***** weird pointer\n",
 	       (int)sizeof(void *));
     }
 #ifdef UINTPTR_MAX
-    printf("Bytes PI%2d, uintptr_t      %"PRIuPTR"\n",
-	    (int)sizeof(uintptr_t), UINTPTR_MAX);
+    printf("Bytes PI%2d, uintptr_t      %"PRIuPTR"\n", (int)sizeof(uintptr_t), UINTPTR_MAX);
 #endif
     printf("Bytes SZ%2d, sizeof()\n", (int)sizeof(sizeof(int)));
     printf("\n");
@@ -164,57 +211,73 @@ main(int argc, char ** argv)
     printf("Bytes I %2d, int            %d\n",
 	    (int)sizeof(int), ((unsigned)-1)>>1 );		/*TWOS*/
 #endif
+
 #if defined(LONG_MAX)
-    printf("Bytes LI%2d, long int       %ld\n",
-	    (int)sizeof(long int), LONG_MAX);
+    printf("Bytes LI%2d, long int       %ld%s%s\n",
+	    (int)sizeof(long int), LONG_MAX,
+	    (sizeof(long)==sizeof(time_t)?" (time_t)":""), ""
+#if defined(LLONG_MAX) && LLONG_MAX == LONG_MAX
+	    " (long long)"
+#define GOT_LLONG
+#endif
+#if defined(INT64_MAX) && INT64_MAX == LONG_MAX
+	    " (int64_t)"
+#define GOT_INT64
+#endif
+#if defined(INTMAX_MAX) && INTMAX_MAX == LONG_MAX
+	    " (intmax_t)"
+#define GOT_INTMAX
+#endif
+	    );
 #else
     printf("Bytes LI%2d, long int       %ld\n",
 	    (int)sizeof(long int), ((unsigned long)-1L)>>1);	/*TWOS*/
 #endif
 
-    printf("\n");
-
-#ifdef LLONG_MAX
-#if !defined(LONG_MAX) || LONG_MAX != LLONG_MAX
-#if !defined(INT64_MAX) || INT64_MAX != LLONG_MAX
-    printf("Bytes LL%2d, long long      %"PRIdLLONG"\n",
-	    (int)sizeof(long long), LLONG_MAX);
+#if defined(LLONG_MAX) && !defined(GOT_LLONG)
+    printf("Bytes LL%2d, long long      %"PRIdLLONG"%s%s\n",
+	    (int)sizeof(long long), LLONG_MAX,
+	    (sizeof(long long)==sizeof(time_t)?" (time_t)":""), ""
+#if defined(INT64_MAX) && INT64_MAX == LLONG_MAX
+	    " (int64_t)"
+#define GOT_INT64
 #endif
-#endif
-#endif
-#ifdef INT64_MAX
-    printf("Bytes I*%2d, int64_t        %"PRId64"%s\n",
-	    (int)sizeof(int64_t), INT64_MAX, ""
-#if defined(LLONG_MAX) && INT64_MAX == LLONG_MAX
-	    " (long long)"
+#if defined(INTMAX_MAX) && INTMAX_MAX == LLONG_MAX
+	    " (intmax_t)"
+#define GOT_INTMAX
 #endif
 	    );
 #endif
-#ifdef INT128_MAX
-    printf("Bytes I*%2d, int128_t       %"PRId128"\n",
-	    (int)sizeof(int128_t), INT128_MAX);
-#endif
-#ifdef INTMAX_MAX
-    printf("Bytes IM%2d, intmax_t       %"PRIdMAX"\n",
-	    (int)sizeof(intmax_t), INTMAX_MAX);
-#ifdef __SIZEOF_INT128__
-    if (sizeof(__int128) > sizeof(intmax_t))
-	printf("Bytes I*%2d, __int128 <**** Larger than intmax_t\n",
-	    (int)sizeof(__int128));
-#endif
+
+#if defined(LONG_LONG_MAX) && !defined(GOT_LLONG)
+    printf("Bytes LL%2d, long long      %llu (non-POSIX)\n",
+	    (int)sizeof(long long), LONG_LONG_MAX);
 #endif
 
-#if defined(INTMAX_MAX)
-    printf("\n");
+#if defined(INT64_MAX) && !defined(GOT_INT64)
+    printf("Bytes I*%2d, int64_t        %"PRId64"\n", (int)sizeof(int64_t), INT64_MAX);
 #endif
+
+#if defined(INT128_MAX) && !defined(GOT_INT128)
+    printf("Bytes I*%2d, int128_t       %"PRId128"\n", (int)sizeof(int128_t), INT128_MAX);
+#endif
+
+#if defined(INTMAX_MAX) && !defined(GOT_INTMAX)
+    printf("Bytes IM%2d, intmax_t       %"PRIdMAX"\n", (int)sizeof(intmax_t), INTMAX_MAX);
+#endif
+
+#if defined(__SIZEOF_INT128__) && defined(INTMAX_MAX)
+    if (sizeof(__int128) > sizeof(intmax_t))
+	printf("Bytes I*%2d, __int128 <**** Larger than intmax_t\n", (int)sizeof(__int128));
+#endif
+
+    printf("\n");
 
 #if defined(USHRT_MAX)
-    printf("Bytes US%2d, unsigned short %u\n",
-	    (int)sizeof(unsigned short), USHRT_MAX);
+    printf("Bytes US%2d, unsigned short %u\n", (int)sizeof(unsigned short), USHRT_MAX);
 #endif
 #if defined(UINT_MAX)
-    printf("Bytes UI%2d, unsigned int   %u\n",
-	    (int)sizeof(unsigned int), UINT_MAX);
+    printf("Bytes UI%2d, unsigned int   %u\n", (int)sizeof(unsigned int), UINT_MAX);
 #else
     {
 	int int_bit, int_min = 0, int_max = 0;
@@ -234,17 +297,14 @@ main(int argc, char ** argv)
     }
 #endif
 #if defined(ULONG_MAX)
-    printf("Bytes UL%2d, unsigned long  %lu\n",
-	    (int)sizeof(unsigned long), ULONG_MAX);
+    printf("Bytes UL%2d, unsigned long  %lu\n", (int)sizeof(unsigned long), ULONG_MAX);
 #else
-    printf("Bytes UL%2d, unsigned long  %lu\n",
-	    (int)sizeof(unsigned long), (unsigned long)-1L);	/*STDC*/
+    printf("Bytes UL%2d, unsigned long  %lu\n", (int)sizeof(unsigned long), (unsigned long)-1L);	/*STDC*/
 #endif
 #if defined(ULLONG_MAX)
 #if !defined(ULONG_MAX) || ULONG_MAX != ULLONG_MAX
 #if !defined(UINT64_MAX) || UINT64_MAX != ULLONG_MAX
-    printf("Bytes LL%2d, unsigned long l%"PRIuLLONG"\n",
-	    (int)sizeof(unsigned long long), ULLONG_MAX);
+    printf("Bytes LL%2d, unsigned long l%"PRIuLLONG"\n", (int)sizeof(unsigned long long), ULLONG_MAX);
 #endif
 #endif
 #endif
@@ -257,8 +317,7 @@ main(int argc, char ** argv)
 	    );
 #endif
 #if defined(UINTMAX_MAX)
-    printf("Bytes UM%2d, uintmax_t      %"PRIuMAX"\n",
-	    (int)sizeof(uintmax_t), UINTMAX_MAX);
+    printf("Bytes UM%2d, uintmax_t      %"PRIuMAX"\n", (int)sizeof(uintmax_t), UINTMAX_MAX);
 #endif
 
 #if defined(UINT_MAX)
@@ -266,18 +325,15 @@ main(int argc, char ** argv)
 #endif
 
 #if defined(FLT_MAX)
-    printf("Bytes FL%2d, float         %g\n",
-	    (int)sizeof(float), FLT_MAX);
+    printf("Bytes FL%2d, float         %g\n", (int)sizeof(float), FLT_MAX);
 #endif
 #if defined(DBL_MAX)
-    printf("Bytes DB%2d, double        %g\n",
-	    (int)sizeof(double), DBL_MAX);
+    printf("Bytes DB%2d, double        %g\n", (int)sizeof(double), DBL_MAX);
 #endif
 
 #if !defined(_WIN32) || defined(__MINGW32__)
 #if defined(LDBL_MAX) && defined(LDBL_MAX_EXP) && defined(DBL_MAX_EXP) && LDBL_MAX_EXP != DBL_MAX_EXP
-    printf("Bytes LD%2d, long double   %Lg\n",
-	    (int)sizeof(long double), LDBL_MAX);
+    printf("Bytes LD%2d, long double   %Lg\n", (int)sizeof(long double), LDBL_MAX);
 #endif
 #endif
 
