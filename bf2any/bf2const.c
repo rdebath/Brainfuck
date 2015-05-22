6ea62ea28740ef63d43f5833466464a647549cf2
Robert de Bath <rdebath@tvisiontech.co.uk>
2015-05-22 20:43:18 +0100

    Cell size limiter gave invalid code.

--- bf2any/bf2const.c
+++ bf2any/bf2const.c
@@ -259,6 +259,10 @@ void outopt(int ch, int count)
     case '=': tape->v = count; tape->is_set = 1; break;
 
     case 'B':
+	/* Some BE are not 32 bits, try to avoid cell size mistakes */
+	if (!cells_are_ints && (tape->v > 65536 || tape->v < -65536))
+	    ;
+	else
 	if (tape->is_set) {
 	    if (bytecell) tape->v %= 256; /* Note: preserves sign but limits range. */
 	    reg_known = 1;
@@ -334,8 +338,4 @@ void outopt(int ch, int count)
 	}
 	if (bytecell) tape->v %= 256; /* Note: preserves sign but limits range. */
     }
-
-    /* Some BE are not 32 bits, try to avoid cell size mistakes */
-    if (!cells_are_ints && (tape->v > 65536 || tape->v < -65536))
-	flush_tape(0,0);
 }
