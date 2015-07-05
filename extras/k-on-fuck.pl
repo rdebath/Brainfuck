a8ff45e2367ce278200f2359793e2cea7f45669e
Robert de Bath <rdebath@tvisiontech.co.uk>
2015-07-05 17:20:05 +0100

    Some more micro interpreters.

---
+++ extras/k-on-fuck.pl
@@ -0,0 +1,2 @@
+%c=qw(AAA $p++ AAB $p-- ABA D++ ABB D-- BBA D&=255;while(D){ BBB D&=255;} BAA print+chrD BAB D=ord(getc));
+$/=$,;$_=<>;s/[AB]//g;s/うん/A/g;s/たん/B/g;s/[^AB]//g;s/.../$c{$&};/g;s[D]'$b[$p]'g;eval
