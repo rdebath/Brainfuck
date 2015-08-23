47853dea7dcade73d7116a72b94fee5f09923734
Robert de Bath <rdebath@tvisiontech.co.uk>
2015-08-23 09:53:20 +0100

    Alter the regexs for recognising Ook commands.
    The two Ooks in a command are matched as one unit but must be delimited by
    a word boundary.  One exception to this is that recent research implies
    that The Librarian may have been saying 'Book' not 'Ook' so this variant
    is also accepted.
    
    The "Ook? Ook?" command is the debugging command that this interpreter
    does not support.

--- extras/ook.pl
+++ extras/ook.pl
@@ -1,2 +1,3 @@
 %c=qw(AC $p++ CA $p-- AA D++ BB D-- BC D&=255;while(D){ CB D&=255;} BA print+chrD AB D=ord(getc));
-$/=$,;$_=<>;s/[ABC]//g;s/Ook\./A/gi;s/Ook!/B/gi;s/Ook\?/C/gi;s/[^ABC]//g;s/../$c{$&};/g;s[D]'$b[$p]'g;eval
+$/=$,;$_=<>;$_=lc$_;s/\bb?ook([.!?])[\t\n ]*b?ook([.!?])/O\1 O\2/g;
+s/O\./A/g;s/O!/B/g;s/O\?/C/g;s/[^ABC]//g;s/../$c{$&};/g;s[D]'$b[$p]'g;eval
