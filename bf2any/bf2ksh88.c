bc396821dd6c0041d7533b781423ea5a2ca4328f
Robert de Bath <rdebath@tvisiontech.co.uk>
2016-05-31 09:02:43 +0100

    Workaround for bug in ksh88-1st

--- bf2any/bf2ksh88.c
+++ bf2any/bf2ksh88.c
@@ -40,7 +40,7 @@ outcmd(int ch, int count)
 	pr("fi");
 
 	pr("");
-	pr("o() { typeset -i8 N ; N=$1; print -n - \\\\0$(print ${N#*#});}");
+	pr("o() { typeset -i8 N ; N=$1; print -n - \\\\0$(print ${N#8?});}");
 	pr("if [ \"`{ o 65;o 65;} 2>&1 </dev/null`\" != AA ]");
 	pr("then");
 	pr("    echo 'ERROR: The print command must be Ksh88 compatible' >&2");
