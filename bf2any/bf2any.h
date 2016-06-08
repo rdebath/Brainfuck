8de62e97c0ace41039705db9f3d194bccd4d4fb0
Robert de Bath <rdebath@tvisiontech.co.uk>
2016-06-08 08:01:49 +0100

    Add the strdup replacement to bf2any

--- bf2any/bf2any.h
+++ bf2any/bf2any.h
@@ -22,3 +22,21 @@ char * get_string(void);
 
 #define tapeinit (enable_optim?BOFF:0)
 #define tapesz   (tapelen+tapeinit)
+
+/* Make a strdup() for old systems with an ok GCC */
+
+#if defined(__GNUC__) \
+    && (__GNUC__ > 2) || (__GNUC__ == 2 && __GNUC_MINOR__ >= 7)
+#define UNUSED __attribute__ ((__unused__))
+
+#if (_XOPEN_VERSION+0) < 500 && _POSIX_VERSION < 200809L
+#if !defined(strdup) && !defined(_WIN32)
+#define strdup(str) \
+    ({char*_s=(str);int _l=strlen(_s)+1;void*_t=malloc(_l);if(_t)memcpy(_t,_s,_l);_t;})
+#endif
+#endif
+
+#else
+#define UNUSED
+#define __attribute__(__ignored__)
+#endif
