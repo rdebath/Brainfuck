7ab8043cd2e1d70c66cc1dde02d76da7cc491eb0
Robert de Bath <rdebath@tvisiontech.co.uk>
2017-10-21 13:00:43 +0100

    Add two more interpreters.
    File broken-bf0.c is a working "Do-While" interpreter, as such it's a
    broken BF interpreter.
    
    File broken-bfF.c is a working BF interpreter but the cell value is
    0..255 without overflows (the values are clamped). As such it's an
    unusual interpreter that I should check for false positives.
    
    Also adapt to screen width.

--- try.sh
+++ try.sh
@@ -4,6 +4,9 @@ if [ ! -n "$BASH_VERSION" ];then exec bash "$0" "$@";else set +o posix;fi
 [ -f "$1" ] || { echo >&2 "Usage: $0 BF_Program"; exit 1;}
 
 P="$(dirname "$0")"
+W=$(( ($(tput cols)-2)/3-8))
+
+[ "$W" -ge 18 ] || W=18
 
 echo "Correct      :$(
 ruby <<\! - "$1" |
@@ -18,13 +21,13 @@ eval 'm=Hash.new(p=0);'+ARGF.read.gsub(
 	'.' => 'putc m[p];',
 	',' => 'm[p]=STDIN.getbyte if !STDIN.eof;')
 !
-xxd -g0 -l18 -c18
+xxd -g0 -l$W -c$W
 )"
 
 for i in "$P"/broken-bf?.c
 do  echo "$(basename "$i")" :"$(
     ( ulimit -t 2 ; tcc -run "$i" "$1" </dev/null |
-    xxd -g0 -l18 -c18 ) 2>&1 )"
+    xxd -g0 -l$W -c$W ) 2>&1 )"
 done
 
 
