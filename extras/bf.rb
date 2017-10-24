ba28690798d5e1211625ba603c2aa40e7bf43d6b
Robert de Bath <rdebath@tvisiontech.co.uk>
2017-10-24 19:58:15 +0100

    Quick upgrade of the Ruby BF

--- extras/bf.rb
+++ extras/bf.rb
@@ -1,6 +1,14 @@
 #!/usr/bin/ruby
-eval 'm=Hash.new(p=0);'+ARGF.read.gsub(
-	/./,
+
+h = Hash.new{|_, k|
+    (k[0] == '+') ? "m[p]+="+k.length.to_s+";\n" :
+    (k[0] == '-') ? "m[p]-="+k.length.to_s+";\n" :
+    (k[0] == '>') ? "p+="+k.length.to_s+";\n" :
+    (k[0] == '<') ? "p-="+k.length.to_s+";\n" :
+    (k[0..3] == '[-]+') ? "m[p]="+(k.length-3).to_s+";\n" :
+    "#{k}\n" };
+
+h.merge!({
 	'>' => 'p+=1;',
 	'<' => 'p-=1;',
 	'+' => 'm[p]+=1;',
@@ -8,4 +16,10 @@ eval 'm=Hash.new(p=0);'+ARGF.read.gsub(
 	'[' => '(',
 	']' => ')while((m[p]&=255)!=0);',
 	'.' => 'putc m[p];',
-	',' => 'm[p]=STDIN.getbyte if !STDIN.eof;')
+	',' => 'm[p]=STDIN.getbyte if !STDIN.eof;',
+	'[-]' => 'm[p]=0;',
+	});
+
+eval 'm=Hash.new(p=0);'+ARGF.read.
+    gsub(/[^\[\]+,-.<>]/,'').
+    gsub(/\[\-\]\+*|\++|-+|>+|<+|./,h);
