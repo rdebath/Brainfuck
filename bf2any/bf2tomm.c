8c72383efbc9110c2fa6ffefb875b36300308b3d
Robert de Bath <rdebath@tvisiontech.co.uk>
2018-01-14 22:07:01 +0000

    Add IF and ENDIF tokens to bf2tomm

--- bf2any/bf2tomm.c
+++ bf2any/bf2tomm.c
@@ -35,7 +35,7 @@ static int use_macro = 0;
 static int bpc = 0;
 
 static check_arg_t fn_check_arg;
-struct be_interface_s be_interface = {.check_arg=fn_check_arg,.nobytecell=1};
+struct be_interface_s be_interface = {.check_arg=fn_check_arg,.nobytecell=1,.ifcmd=1};
 
 static int
 fn_check_arg(const char * arg)
@@ -279,6 +279,17 @@ outcmd(int ch, int count)
 
     case ']':
 	I; printf("mp_mask_BPC(p);\n");
+	ind--;
+	I; printf("}\n");
+	break;
+
+    case 'I':
+	I; printf("mp_mask_BPC(p);\n");
+	I; printf("if(mp_cmp_d(p, 0) != MP_EQ){\n");
+	ind++;
+	break;
+
+    case 'E':
 	ind--;
 	I; printf("}\n");
 	break;
