5d3b14e3105c41534314228e1864ea675580a5d0
Robert de Bath <rdebath@tvisiontech.co.uk>
2018-04-05 08:05:44 +0100

    Regenerate NASM header without tabs

--- trivial-bf2nasm.c
+++ trivial-bf2nasm.c
@@ -2,65 +2,7 @@
 #include <string.h>
 
 
-char * header =
-	"; asmsyntax=nasm\n"
-	"; nasm -f bin bf.s && chmod +x -\n"
-	"\n"
-	"BITS 32\n"
-	"\n"
-	"memsize\tequ\t0x10000\n"
-	"orgaddr\tequ\t0x08048000\n"
-	"\torg\torgaddr\n"
-	"\n"
-	"; A nice legal ELF header here, bit short, but that's okay.\n"
-	"; I chose this as the smallest completely legal one from ...\n"
-	"; http://www.muppetlabs.com/~breadbox/software/tiny/teensy.html\n"
-	"\n"
-	"ehdr:\t\t\t\t\t\t; Elf32_Ehdr\n"
-	"\tdb\t0x7F, \"ELF\", 1, 1, 1, 0\t\t;   e_ident\n"
-	"\ttimes 8 db\t0\n"
-	"\tdw\t2\t\t\t\t;   e_type\n"
-	"\tdw\t3\t\t\t\t;   e_machine\n"
-	"\tdd\t1\t\t\t\t;   e_version\n"
-	"\tdd\t_start\t\t\t\t;   e_entry\n"
-	"\tdd\tphdr - $$\t\t\t;   e_phoff\n"
-	"\tdd\t0\t\t\t\t;   e_shoff\n"
-	"\tdd\t0\t\t\t\t;   e_flags\n"
-	"\tdw\tehdrsize\t\t\t;   e_ehsize\n"
-	"\tdw\tphdrsize\t\t\t;   e_phentsize\n"
-	"\tdw\t1\t\t\t\t;   e_phnum\n"
-	"\tdw\t0\t\t\t\t;   e_shentsize\n"
-	"\tdw\t0\t\t\t\t;   e_shnum\n"
-	"\tdw\t0\t\t\t\t;   e_shstrndx\n"
-	"\n"
-	"ehdrsize\tequ\t$ - ehdr\n"
-	"\n"
-	"phdr:\t\t\t\t\t\t; Elf32_Phdr\n"
-	"\tdd\t1\t\t\t\t;   p_type\n"
-	"\tdd\t0\t\t\t\t;   p_offset\n"
-	"\tdd\t$$\t\t\t\t;   p_vaddr\n"
-	"\tdd\t$$\t\t\t\t;   p_paddr\n"
-	"\tdd\tfilesize\t\t\t;   p_filesz\n"
-	"\tdd\tfilesize+memsize\t\t;   p_memsz\n"
-	"\tdd\t7\t\t\t\t;   p_flags\n"
-	"\tdd\t0x1000\t\t\t\t;   p_align\n"
-	"\n"
-	"phdrsize\tequ\t$ - phdr\n"
-	"\n"
-	"\tsection\t.text\n"
-	"\tsection\t.data align=64\n"
-	"\tsection\t.bss align=64\n"
-	"filesize equ\tsection..bss.start-orgaddr\n"
-	"tape:\n"
-	"\n"
-	"\tsection\t.text\n"
-	"_start:\n"
-	"xor eax, eax\n"
-	"cdq\n"
-	"inc edx\n"
-	"mov ecx, tape\n"
-	;
-
+extern char * header;
 char * bf = "+-><[].,";
 
 /* These translations are the same as used by:
@@ -77,15 +19,13 @@ char * cc[] = {
 	"mov al, 3\nxor ebx, ebx\nint 0x80"
 };
 
-char * footer = "xchg eax, edx\nxor ebx, ebx\nint 0x80\n" ;
-
 int main(int argc, char ** argv) {
 	char * s;
 	int c;
 	int loopid = 1;
 	int sp=0, stk[999];
 
-	printf("%s", header);
+	printf("%s\n", header);
 
 	while((c=getchar()) != EOF)
 		if ((s = strchr(bf, c))) {
@@ -99,7 +39,85 @@ int main(int argc, char ** argv) {
 			} else
 				printf("%s\n", cc[s-bf]);
 		}
-
-	printf("%s", footer);
 	return 0;
 }
+
+char * header =
+	"; asmsyntax=nasm"
+"\n"	"; nasm brainfuck.s && chmod +x brainfuck"
+"\n"
+"\n"	"BITS 32"
+"\n"
+"\n"	"memsize equ     0x100000"
+"\n"	"orgaddr equ     0x08048000"
+"\n"	"        org     orgaddr"
+"\n"
+"\n"	"; A nice legal ELF header here, bit short, but that's okay."
+"\n"	"; Based on the headers from ..."
+"\n"	"; http://www.muppetlabs.com/~breadbox/software/tiny/teensy.html"
+"\n"	"; but with RX and RW segments. See also ..."
+"\n"	"; http://blog.markloiseau.com/2012/05/tiny-64-bit-elf-executables"
+"\n"
+"\n"	"ehdr:                                   ; Elf32_Ehdr"
+"\n"	"        db      0x7F, \"ELF\", 1, 1, 1, 0 ;   e_ident"
+"\n"	"        times 8 db      0"
+"\n"	"        dw      2                       ;   e_type"
+"\n"	"        dw      3                       ;   e_machine"
+"\n"	"        dd      1                       ;   e_version"
+"\n"	"        dd      _start                  ;   e_entry"
+"\n"	"        dd      phdr - $$               ;   e_phoff"
+"\n"	"        dd      0                       ;   e_shoff"
+"\n"	"        dd      0                       ;   e_flags"
+"\n"	"        dw      ehdrsize                ;   e_ehsize"
+"\n"	"        dw      phdrsize                ;   e_phentsize"
+"\n"	"        dw      2                       ;   e_phnum"
+"\n"	"        dw      40                      ;   e_shentsize"
+"\n"	"        dw      0                       ;   e_shnum"
+"\n"	"        dw      0                       ;   e_shstrndx"
+"\n"
+"\n"	"ehdrsize        equ     $ - ehdr"
+"\n"
+"\n"	"phdr:                                   ; Elf32_Phdr"
+"\n"	"        dd      1                       ;   p_type"
+"\n"	"        dd      0                       ;   p_offset"
+"\n"	"        dd      $$                      ;   p_vaddr"
+"\n"	"        dd      0                       ;   p_paddr"
+"\n"	"        dd      filesize                ;   p_filesz"
+"\n"	"        dd      ramsize                 ;   p_memsz"
+"\n"	"        dd      5                       ;   p_flags"
+"\n"	"        dd      0x1000                  ;   p_align"
+"\n"
+"\n"	"phdrsize        equ     $ - phdr"
+"\n"
+"\n"	"        dd      1                       ;   p_type"
+"\n"	"        dd      0                       ;   p_offset"
+"\n"	"        dd      section..bss.start      ;   p_vaddr"
+"\n"	"        dd      0                       ;   p_paddr"
+"\n"	"        dd      0                       ;   p_filesz"
+"\n"	"        dd      memsize                 ;   p_memsz"
+"\n"	"        dd      6                       ;   p_flags"
+"\n"	"        dd      0x1000                  ;   p_align"
+"\n"
+"\n"	"        section .text"
+"\n"	"        section .bftext align=64"
+"\n"	"        section .bftail align=1"
+"\n"
+"\n"	"exit_prog:"
+"\n"	"        mov     bl, 0           ; Exit status"
+"\n"	"        xor     eax, eax"
+"\n"	"        inc     eax             ; syscall 1, exit"
+"\n"	"        int     0x80            ; exit(0)"
+"\n"
+"\n"	"        section .bflast align=1"
+"\n"	"        section .bss align=4096"
+"\n"	"ramsize equ     section..bss.start-orgaddr"
+"\n"	"filesize equ    section..bflast.start-orgaddr"
+"\n"	"mem:"
+"\n"
+"\n"	"        section .bftext"
+"\n"	"_start:"
+"\n"	"        xor     eax, eax        ; EAX = 0 ;don't change high bits."
+"\n"	"        cdq                     ; EDX = 0 ;sign bit of EAX"
+"\n"	"        inc     edx             ; EDX = 1 ;ARG4 for system calls"
+"\n"	"        mov     ecx,mem"
+;
