22e88fe6b006953b611166b91c2378144ae00add
Robert de Bath <rdebath@tvisiontech.co.uk>
2018-08-12 14:13:32 +0100

    A new way of breaking +/-

---
+++ broken-bfI.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+int main (int argc, char *argv[]) {
+    char *b=0, *p, t[65536]={0};
+    unsigned short m=0;
+    int i=0;
+    FILE *fp=argc>1?fopen(argv[1], "r"):stdin;
+    size_t r=0;
+    t[60000] = 4;
+    if(!fp || getdelim(&b,&r,argc>1?'\0':'!',fp)<0)
+	perror(argv[1]);
+    else if(b&&r>0)for(p=b;*p;p++)switch(*p) {
+	case '>': m++; break;
+	case '<': m--; break;
+	case '.': putchar(t[m]);break;
+	case ',': {int c=getchar();if(c!=EOF)t[m]=c;}break;
+	case '[': if(t[m]==0)while((i+=(*p=='[')-(*p==']'))&&p[1])p++;break;
+	case ']': if(t[m]!=0)while((i+=(*p==']')-(*p=='['))&&p>b)p--;break;
+
+	case '+': t[m] = (t[m] < 255 ? t[m] + 1 : 0); break;
+	case '-': t[m] = (t[m] > 0 ? t[m] - 1 : 29999); break;
+    }
+    return 0;
+}
