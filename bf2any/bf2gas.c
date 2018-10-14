50f7d3a72dbd2cf864ddd267dd8fe8e702925e16
Robert de Bath <rdebath@tvisiontech.co.uk>
2018-10-14 14:55:30 +0100

    Get rid of distinct bf2gas32 and bf2gas64
    Now there's just one executable produced and the four variants are
    selectable with options.

--- bf2any/bf2gas.c
+++ bf2any/bf2gas.c
@@ -12,31 +12,31 @@
  *
  * Compile output with: gcc -o bfp bfp.s
  * Add -m32 or -m64 option if it's not your default target arch.
+ *
+ * Note: gcc -m64 -S -masm=intel -fno-asynchronous-unwind-tables example.c
  */
 
-static int ind = 0;
-
-#if defined(__FreeBSD__) && !defined(GLABELS)
-#define GLABELS
+#if !defined(USE64)
+#if !defined(__amd64__)
+#define USE64	0
+#else
+#define USE64	1
+#endif
 #endif
 
-#if !defined(USE32) && !defined(USE64) && defined(__i386__)
-#define USE32
-#endif
-
-#ifdef USE32
 /* Call params on stack
  * Callee saves: %ebp, %ebx, %esi, %edi
  * Caller saves: %eax, %ecx, and %edx
  */
 
-#define AX	"eax"
-#define BX	"ebx"
-#define CX	"ecx"
-#define DX	"edx"
-#define BP	"ebp"
-#define SP	"esp"
-#else
+#define AX32	"eax"
+#define BX32	"ebx"
+#define CX32	"ecx"
+#define DX32	"edx"
+#define BP32	"ebp"
+#define SP32	"esp"
+#define DI32	"edi"
+
 /* Call params in registers: %rdi, %rsi, %rdx, %rcx, %r8 and %r9
  * MSWindows ONLY Call params in registers: %rcx, %rdx, %r8 and %r9
  * Callee saves: %rbp, %rbx and %r12 through %r15
@@ -45,20 +45,48 @@ static int ind = 0;
  * Temps: %eax (set to zero for most FN calls)
  */
 
-#define AX	"eax"
-#define BX	"rbx"
-#define CX	"rcx"
-#define DX	"rdx"
-#define BP	"rbp"
-#define SP	"rsp"
-#define DI	"edi"
-#endif
+#define AX64	"eax"
+#define BX64	"rbx"
+#define CX64	"rcx"
+#define DX64	"rdx"
+#define BP64	"rbp"
+#define SP64	"rsp"
+#define DI64	"edi"
 
-#ifdef GLABELS
+#define AX	(use_64bit?AX64:AX32)
+#define BX	(use_64bit?BX64:BX32)
+#define CX	(use_64bit?CX64:CX32)
+#define DX	(use_64bit?DX64:DX32)
+#define BP	(use_64bit?BP64:BP32)
+#define SP	(use_64bit?SP64:SP32)
+#define DI	(use_64bit?DI64:DI32)
+
+static int ind = 0, text_labels = 1, use_64bit = USE64;
 struct stkdat { struct stkdat * up; int id; } *sp = 0;
-#endif
 
-struct be_interface_s be_interface = {.bytesonly=1,.disable_be_optim=1};
+static check_arg_t fn_check_arg;
+struct be_interface_s be_interface = {.bytesonly=1,.disable_be_optim=1,
+    .check_arg=fn_check_arg};
+
+static int
+fn_check_arg(const char * arg)
+{
+    if (strcmp(arg, "-64") == 0) { use_64bit = 1; return 1; }
+    if (strcmp(arg, "-32") == 0) { use_64bit = 0; return 1; }
+    if (strcmp(arg, "-static") == 0) { text_labels = 1; return 1; }
+    if (strcmp(arg, "-local") == 0) { text_labels = 0; return 1; }
+
+    if (strcmp("-h", arg) ==0) {
+	fprintf(stderr, "%s\n",
+	"\t"	"-64     x86 64 bit code"
+	"\n\t"	"-32     x86 32 bit code"
+	"\n\t"	"-static Use normal static labels"
+	"\n\t"	"-local  Use numeric local labels"
+	);
+	return 1;
+    }
+    return 0;
+}
 
 void
 outcmd(int ch, int count)
@@ -70,22 +98,25 @@ outcmd(int ch, int count)
 	puts(".text");
 	puts(".globl main");
 	puts("main:");
-	puts("push "BP);
-	puts("mov "BP", "SP);
-	puts("push "BX);
-	puts("mov "BX", offset flat:buffer");
+	printf("push %s\n", BP);
+	printf("mov %s, %s\n", BP, SP);
+	printf("push %s\n", BX);
+	if (use_64bit)
+	    printf("lea %s, buffer[rip]\n", BX);
+	else
+	    printf("mov %s, offset flat:buffer\n", BX);
 	if (tapeinit)
-	    printf("add "BX", %d\n", tapeinit);
+	    printf("add %s, %d\n", BX, tapeinit);
 	break;
-    case '=': printf("mov byte ptr ["BX"], %d\n", count & 0xFF); break;
-    case '+': printf("add byte ptr ["BX"], %d\n", count); break;
-    case '-': printf("sub byte ptr ["BX"], %d\n", count); break;
-    case '<': printf("sub "BX", %d\n", count); break;
-    case '>': printf("add "BX", %d\n", count); break;
+    case '=': printf("mov byte ptr [%s], %d\n", BX, count & 0xFF); break;
+    case '+': printf("add byte ptr [%s], %d\n", BX, count); break;
+    case '-': printf("sub byte ptr [%s], %d\n", BX, count); break;
+    case '<': printf("sub %s, %d\n", BX, count); break;
+    case '>': printf("add %s, %d\n", BX, count); break;
     case '[':
-	puts("movzx "AX", byte ptr ["BX"]");
-	puts("test "AX", "AX);
-#ifdef GLABELS
+	printf("movzx %s, byte ptr [%s]\n", AX, BX);
+	printf("test %s, %s\n", AX, AX);
+	if (text_labels)
 	{
 	    struct stkdat * n = malloc(sizeof*n);
 	    n->up = sp;
@@ -94,16 +125,17 @@ outcmd(int ch, int count)
 	    printf("jz LE%d\n", n->id);
 	    printf("LB%d:\n", n->id);
 	}
-#else
-	printf("jz %df\n", ind*2+11);
-	printf("%d:\n", ind*2+10);
-	ind++;
-#endif
+	else
+	{
+	    printf("jz %df\n", ind*2+11);
+	    printf("%d:\n", ind*2+10);
+	    ind++;
+	}
 	break;
     case ']':
-	puts("movzx "AX", byte ptr ["BX"]");
-	puts("test "AX", "AX);
-#ifdef GLABELS
+	printf("movzx %s, byte ptr [%s]\n", AX, BX);
+	printf("test %s, %s\n", AX, AX);
+	if (text_labels)
 	{
 	    struct stkdat * n = sp;
             sp = n->up;
@@ -111,32 +143,37 @@ outcmd(int ch, int count)
 	    printf("LE%d:\n", n->id);
             free(n);
 	}
-#else
-	ind--;
-	printf("jnz %db\n", ind*2+10);
-	printf("%d:\n", ind*2+11);
-#endif
+	else
+	{
+	    ind--;
+	    printf("jnz %db\n", ind*2+10);
+	    printf("%d:\n", ind*2+11);
+	}
 	break;
     case ',':
-	puts("call getchar");
-	puts("cmp "AX", -1");
+	if (use_64bit)
+	    puts("call getchar@PLT");
+	else
+	    puts("call getchar");
+	printf("cmp %s, -1\n", AX);
 	printf("je 1f\n");
-	puts("mov byte ptr ["BX"], al");
+	printf("mov byte ptr [%s], al\n", BX);
 	printf("1:\n");
 	break;
     case '.':
-	puts("movzx "AX", byte ptr ["BX"]");
-#ifdef USE32
-	puts("push "AX);
-	puts("call putchar");
-	puts("pop "AX);
-#else
-	puts("mov "DI", "AX);
-	puts("call putchar");
-#endif
+	printf("movzx %s, byte ptr [%s]\n", AX, BX);
+	if (!use_64bit)
+	{
+	    printf("push %s\n", AX);
+	    puts("call putchar");
+	    printf("pop %s\n", AX);
+	} else {
+	    printf("mov %s, %s\n", DI, AX);
+	    puts("call putchar@PLT");
+	}
 	break;
     case '~':
-	puts("pop "BX);
+	printf("pop %s\n", BX);
 	puts("leave");
 	puts("ret");
 	printf(".comm buffer,%d,32\n", tapesz);
