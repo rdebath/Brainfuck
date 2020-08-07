9ac0faafe224a2a4c15d3cb41ddc719881974caf
Robert de Bath <rdebath@tvisiontech.co.uk>
2020-08-07 18:41:44 +0100

    Fix use of malloc rather than calloc in microbf.c

--- extras/microbf.c
+++ extras/microbf.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 int main (int argc, char *argv[]) {
     unsigned short m=0; int i=0, n=0, s=0, g=0, c;
-    char *b = malloc(s=1024), *p, t[65536]={0};
+    char *b = calloc(s=1024,1), *p, t[65536]={0};
     FILE *fp=argc>1?fopen(argv[1], "r"):stdin;
-    if(fp)
-	while ((c=getc(fp)) != EOF && (c!='!' || argc>1 || !g)) {
-	    if (n+2>s && !(b = realloc(b, s += 1024))) break;
-	    g |= ((b[n++] = c) == ','); b[n] = 0;
-	}
+    while (fp && (c=getc(fp)) != EOF && (c!='!' || argc>1 || !g)) {
+	if (n+2>s && !(b = realloc(b, s += 1024))) break;
+	if (c) g |= ((b[n++] = c) == ','); b[n] = 0;
+    }
     if(!fp || !b) { perror(argv[1]); return 1;}
     for(p=b;*p;p++)switch(*p) {
 	case '>': m++;break;
